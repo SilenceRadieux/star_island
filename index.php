46848abeb38585d7f07996ad200a63d5c2b927c7
SilenceRadieux <quentin.graff@hotmail.fr>
2023-06-16 09:45:23 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!Doctype html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css"> 
+<title>Décompte avant l'ouverture du site</title>
+</head>
+<body>
+  <div id="logo_starisland">
+    <img src="../assets/pack_graphique/starisland.png" alt="logo_starisland" height="100" >
+  </div>
+<main class="class-main">
+
+  <div class="section-timer"> 
+    <p id="countdown"></p>
+  </div>
+  <div id="text_countdown">
+    <h2> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem minima magnam mollitia illum nostrum quia quasi tempora, quaerat accusantium? Eum, dignissimos nam error a culpa voluptatibus et rerum! Veritatis, odit.</h2>
+  </div>
+
+
+</main>
+
+
+<footer>
+  <button id="btn-discord" class="btn btn-primary" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom"><img src="../assets/pack_graphique/Discord.png" alt="Boutton d'accès aux réseaux sociaux" height="100"></button>
+
+  <div id="offcanvasBottom" class="offcanvas offcanvas-bottom" tabindex="-1">
+    <div class="offcanvas-header">
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div id="logos_reseaux" class="offcanvas-body small">
+      <img src="../assets/pack_graphique/Discord.png" alt="discord" height="100">
+      <img src="../assets/pack_graphique/logo_twitter.png" alt="twitter" height="100">
+      <img src="../assets/pack_graphique/logo_twitch.png" alt="twitch" height="100">
+      <img src="../assets/pack_graphique/logo_youtube.png" alt="youtube" height="100">
+      <img src="../assets/pack_graphique/logo_instagram.png" alt="instagram" height="100">
+      <img src="../assets/pack_graphique/logo_tiktok.png" alt="tiktok" height="100">
+    </div>
+  </div>
+</footer>
+
+<script src="script.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+</body>
+</html>
