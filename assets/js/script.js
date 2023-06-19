96b4c00f0dc8ea8f9dd21af84d5e3bc4f8596010
SilenceRadieux <quentin.graff@hotmail.fr>
2023-06-19 16:16:32 +0200

    gallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,27 @@ if (actualPage === "http://localhost/star_island_final/serveur.php") {
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundRepeat = "no-repeat";
 }
+
+// Récupère tous les éléments conteneurs d'images
+const imageContainers = document.querySelectorAll('.image-container');
+
+// Récupère la lightbox et l'image en grand format
+const lightbox = document.getElementById('lightbox');
+const lightboxImage = document.getElementById('lightbox-image');
+
+// Attache un gestionnaire d'événements de clic à chaque élément conteneur d'image
+imageContainers.forEach(function(container) {
+  container.addEventListener('click', function() {
+    // Affiche la lightbox et met à jour l'URL de l'image en grand format
+    lightbox.style.display = 'block';
+    lightboxImage.src = container.src;
+  });
+});
+
+// Gestionnaire d'événements de clic pour la fermeture de la lightbox
+lightbox.addEventListener('click', function(e) {
+  // Ferme la lightbox lorsque l'utilisateur clique en dehors de l'image en grand format
+  if (e.target === this || e.target.id === 'close-button') {
+    lightbox.style.display = 'none';
+  }
+});
--- inc/body.galerie.inc.php
+++ inc/body.galerie.inc.php
@@ -1,15 +1,124 @@
+<main>
 <h1 id="title_main" >BIENVENUE SUR STAR'ISLAND</h1>
 
-<!-- <div class="container">
-  <div class="carousel">
-    <div class="carousel__face"><span></span></div>
-    <div class="carousel__face"><span></span></div>
-    <div class="carousel__face"><span></span></div>
-    <div class="carousel__face"><span></span></div>
-    <div class="carousel__face"><span></span></div>
-    <div class="carousel__face"><span></span></div>
-    <div class="carousel__face"><span></span></div>
-    <div class="carousel__face"><span></span></div>
-    <div class="carousel__face"><span></span></div>
+<div class="scroll-container">
+<div class="grid image-grid">
+
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img1 image-container"  src="./assets/pack_graphique/star_islannd/amanda.png">
+      </a>
+    </div>
   </div>
-</div> -->
+
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img2 image-container" src="./assets/pack_graphique/star_islannd/charmilia.png">
+      </a>
+    </div>
+  </div>
+
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img3 image-container" src="./assets/pack_graphique/star_islannd/ellios.png">
+      </a>
+    </div>
+  </div>
+
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img4 image-container" src="./assets/pack_graphique/star_islannd/esmeralda.png">
+      </a>
+    </div>
+  </div>
+
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img5 image-container" src="./assets/pack_graphique/star_islannd/hans.png">
+      </a>
+    </div>
+  </div>
+
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img6 image-container" src="./assets/pack_graphique/star_islannd/raoul.png">
+      </a>
+    </div>
+  </div>
+
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img7 image-container" src="./assets/pack_graphique/star_islannd/conceptrice.png">
+      </a>
+    </div>
+  </div>
+
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img8 image-container" src="./assets/pack_graphique/star_islannd/moto1.png">
+      </a>
+    </div>
+  </div>
+
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img9 image-container" src="./assets/pack_graphique/star_islannd/paysage1.png">
+      </a>
+    </div>
+  </div>
+
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img10 image-container" src="./assets/pack_graphique/star_islannd/paysage2.png">
+      </a>
+    </div>
+  </div>
+
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img10 image-container" src="./assets/pack_graphique/star_islannd/paysage2.png">
+      </a>
+    </div>
+  </div>
+
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img10 image-container" src="./assets/pack_graphique/star_islannd/paysage2.png">
+      </a>
+    </div>
+  </div>
+
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img10 image-container" src="./assets/pack_graphique/star_islannd/paysage2.png">
+      </a>
+    </div>
+  </div>
+  <div class="grid-block">
+    <div class="tile">
+      <a class="tile-link" href="#">
+        <img class="tile-img tile-img10 image-container" src="./assets/pack_graphique/star_islannd/paysage2.png">
+      </a>
+    </div>
+  </div>
+</div>
+</div>
+<div class="lightbox" id="lightbox">
+    <span class="close-button" id="close-button">&times;</span>
+    <img src="" alt="Image en grand format" id="lightbox-image">
+  </div>
+  <div class="background"></div>
+</main>
